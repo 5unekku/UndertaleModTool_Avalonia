6d66491fce7b35760294cc8bdd3d90940d6eda0f
5unekku <jimmy.bungalo8@gmail.com>
2026-06-29 21:15:29 -0400

    phase 6: port ExtensionFunction + ExtensionFile editors (editable arg/function grids)

---
+++ UndertaleModToolAvalonia/Editors/UndertaleExtensionFileEditor.axaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Avalonia.Interactivity;
+using UndertaleModLib;
+using UndertaleModLib.Models;
+
+namespace UndertaleModTool
+{
+    public partial class UndertaleExtensionFileEditor : DataUserControl
+    {
+        private static MainWindow mainWindow => MainWindow.Instance;
+
+        public UndertaleExtensionFileEditor()
+        {
+            InitializeComponent();
+            FunctionsList.AddingNewItem += DataGrid_AddingNewItem;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            FunctionsList.AddNew();
+        }
+
+        private void DataGrid_AddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            var itemList = FunctionsList.ItemsSource as IList<UndertaleExtensionFunction>;
+            int lastItem = itemList.Count;
+
+            UndertaleExtensionFunction obj = new()
+            {
+                Name = mainWindow.Data.Strings.MakeString($"new_extension_function_{lastItem}"),
+                ExtName = mainWindow.Data.Strings.MakeString($"new_extension_function_{lastItem}_ext"),
+                RetType = UndertaleExtensionVarType.Double,
+                Arguments = new UndertaleSimpleList<UndertaleExtensionFunctionArg>(),
+                Kind = 11, // ???
+                ID = mainWindow.Data.ExtensionFindLastId()
+            };
+
+            e.NewItem = obj;
+        }
+    }
+}
---
+++ UndertaleModToolAvalonia/Editors/UndertaleExtensionFunctionEditor.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Interactivity;
+
+namespace UndertaleModTool
+{
+    public partial class UndertaleExtensionFunctionEditor : DataUserControl
+    {
+        public UndertaleExtensionFunctionEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            ArgumentTypesList.AddNew();
+        }
+    }
+}
