4014e956512a0f892c8eb5d93a255eae1e79c99d
5unekku <jimmy.bungalo8@gmail.com>
2026-06-29 21:26:07 -0400

    phase 6: port ParticleSystem editor (emitters grid)

---
+++ UndertaleModToolAvalonia/Editors/UndertaleParticleSystemEditor.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Interactivity;
+
+namespace UndertaleModTool
+{
+    public partial class UndertaleParticleSystemEditor : DataUserControl
+    {
+        public UndertaleParticleSystemEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            EmitterListGrid.AddNew();
+        }
+    }
+}
