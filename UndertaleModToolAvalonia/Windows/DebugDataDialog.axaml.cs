2534eb241cc8d6d8d04de0f0a7d07ccc34e24c59
5unekku <jimmy.bungalo8@gmail.com>
2026-06-29 22:29:01 -0400

    phase 8: port DebugDataDialog

---
+++ UndertaleModToolAvalonia/Windows/DebugDataDialog.axaml.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace UndertaleModTool
+{
+    public partial class DebugDataDialog : Window
+    {
+        public enum DebugDataMode
+        {
+            FullAssembler,
+            PartialAssembler,
+            Decompiled,
+            NoDebug
+        }
+
+        public DebugDataMode Result { get; private set; } = DebugDataMode.NoDebug;
+
+        public DebugDataDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Result = DebugDataMode.Decompiled;
+            Close();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Result = DebugDataMode.PartialAssembler;
+            Close();
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Result = DebugDataMode.FullAssembler;
+            Close();
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Result = DebugDataMode.NoDebug;
+            Close();
+        }
+    }
+}
