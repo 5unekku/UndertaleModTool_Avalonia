718846b561d3f7d1e823b4742a2f338bc44ad3c6
5unekku <jimmy.bungalo8@gmail.com>
2026-06-29 21:18:31 -0400

    phase 6: port UndertaleTexturePageItemDisplay control

---
+++ UndertaleModToolAvalonia/Controls/UndertaleTexturePageItemDisplay.axaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+
+namespace UndertaleModTool
+{
+    public partial class UndertaleTexturePageItemDisplay : UserControl
+    {
+        public static readonly StyledProperty<bool> DisplayBorderProperty =
+            AvaloniaProperty.Register<UndertaleTexturePageItemDisplay, bool>(nameof(DisplayBorder), true, defaultBindingMode: BindingMode.TwoWay);
+
+        public bool DisplayBorder
+        {
+            get => GetValue(DisplayBorderProperty);
+            set => SetValue(DisplayBorderProperty, value);
+        }
+
+        public UndertaleTexturePageItemDisplay()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == DisplayBorderProperty && RenderAreaBorder is not null)
+                RenderAreaBorder.BorderThickness = new Thickness(DisplayBorder ? 1 : 0);
+        }
+    }
+}
